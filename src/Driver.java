920c8b1d716d712a685e050e299274e74ccc5b67
kim <kimberly030319@gmail.com>
2024-09-20 06:57:54 +0800

    Uploaded everything

---
+++ src/Driver.java
@@ -0,0 +1,11 @@
+//main method class
+public class Driver {
+	public static void main (String [] args) {
+		Game game = new Game();
+		game.startGame();
+		game.runGame();
+		game.endGame();
+        game.printTop5Scores();
+	}
+
+}
